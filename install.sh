3fb877a00590cb6c8730d0be9cbfb4e5fae64b86
Sophia Blair <sophb.ccjt@proton.me>
2026-03-04 08:50:03 -0300

    add failsafe for PATH

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_URL="https://raw.githubusercontent.com/sophb-ccjt/path-cli/main/path.sh"
+mkdir -p "$HOME/.local/bin"
 curl -fsSL "$SCRIPT_URL" -o "$HOME/.local/bin/path" && chmod +x "$HOME/.local/bin/path"
 echo "path CLI installed successfully!!"
 echo "you can now use the 'path' command to manage your PATH environment variable."
