58702b5506b57e61701db3f4f8f6a2720c516e67
Sophia Blair <sophb.ccjt@proton.me>
2026-03-04 13:10:39 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Installing path..."
 SCRIPT_URL="https://raw.githubusercontent.com/sophb-ccjt/path-cli/main/path.sh"
 INSTALL_DIR="$HOME/.local/bin"
 INSTALL_PATH="$INSTALL_DIR/path"
 
 mkdir -p "$INSTALL_DIR"
 
+echo
 curl -fsSL "$SCRIPT_URL" -o "$INSTALL_PATH"
 chmod +x "$INSTALL_PATH"
 
