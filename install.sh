436016142b43ccbbabd1b8544b5795c9d9f9cd1a
Sophia Blair <sophb.ccjt@proton.me>
2026-03-04 08:45:56 -0300

    Add install script for path CLI
    Install path CLI by downloading script and setting permissions.

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env/ bash
+
+SCRIPT_URL="https://raw.githubusercontent.com/sophb-ccjt/path-cli/main/path.sh"
+curl -fsSL "$SCRIPT_URL" -o "$HOME/.local/bin/path" && chmod +x "$HOME/.local/bin/path"
+echo "path CLI installed successfully!!"
+echo "you can now use the 'path' command to manage your PATH environment variable."
